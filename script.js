32ceb812fa39533ec64be6742a84c9bd90ccd4e9
vitalya <vitalay.efimov@gmail.com>
2025-04-21 08:25:53 +0500

    task3

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const b = document.querySelector('.b-1')
 const f1 = () => {
  alert('task 1')
 }
+const b2 = document.querySelector('.b-2')
+const f2 = () => {
+ alert('task 2')
+}
+
+document.querySelector('.b2').addEventListener('click', function () {
+  alert('Задача 2');
+});
+
+document.querySelector('.p-3').addEventListener("click", () => alert("Задача 3"))
