67cf3fa9029f510f6bf0a4478748ad0a25733121
vitalya <vitalay.efimov@gmail.com>
2025-04-21 22:48:59 +0500

    task4

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ document.querySelector('.b2').addEventListener('click', function () {
 });
 
 document.querySelector('.p-3').addEventListener("click", () => alert("Задача 3"))
+
+const i4 = document.querySelector('.i-4')
+const b4 = document.querySelector('.b-4')
+const out4 = document.querySelector('.out-4')
+
+// const f4 = () => {
+//   const isChecked = checkbox.checked;
+//   if (i4.checked) {
+//     out4.innerHTML = true
+//   } else {
+//     out4.innerHTML = false
+//   }
+// }
+const btn =  document.querySelector('.b-4')
+const checkbox = document.querySelector('.i-4');
+const out = document.querySelector('.out-4')
+btn.addEventListener('click', function () {
+ out.innerHTML = checkbox.checked;  
+});
+
